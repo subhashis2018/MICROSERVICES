784272eff3b9e31b34494f28ff8f43c17b9d7581
subhashis.sarangi@gmail.com <subhashis.sarangi@gmail.com>
2022-08-27 17:11:24 +0530

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

---
+++ limis-service/src/main/java/com/myapp/LimisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimisServiceApplication.class, args);
+	}
+
+}
